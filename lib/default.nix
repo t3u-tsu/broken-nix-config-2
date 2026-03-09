fd26d332af3d0b0fc6d1f4968c41ea9c21722ab5
t3u <t3u@t3u.uk>
2026-03-09 21:05:40 +0900

    fix: add home-manager backupFileExtension to resolve config conflicts

--- lib/default.nix
+++ lib/default.nix
@@ -17,6 +17,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
           home-manager.extraSpecialArgs = { inherit inputs; };
           home-manager.sharedModules = [
             inputs.nix-index-database.homeModules.nix-index
