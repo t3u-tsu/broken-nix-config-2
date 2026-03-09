f7a2bd8d44fdae090e960b9325e9b8b0128f9cec
t3u <t3u@t3u.uk>
2026-03-09 20:40:53 +0900

    fix(fcitx5): enable waylandFrontend and add plasma6/qt6 integration for pretty and working input

--- modules/services/desktop/fcitx5.nix
+++ modules/services/desktop/fcitx5.nix
@@ -4,17 +4,14 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-mozc
-      fcitx5-gtk
-      kdePackages.fcitx5-configtool
-    ];
-  };
-
-  # Environment variables for Fcitx5
-  environment.variables = {
-    GTK_IM_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
-    XMODIFIERS = "@im=fcitx";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [
+        fcitx5-mozc
+        kdePackages.fcitx5-qt
+        fcitx5-gtk
+        kdePackages.fcitx5-configtool
+      ];
+    };
   };
 }
