84c4d54d8451e2764350b6e592a174c331e7bc86
t3u-tsu <t3u@t3u.uk>
2026-03-16 19:16:14 +0900

    chore: remove the dark reader extention

--- modules/home/desktop/browsers.nix
+++ modules/home/desktop/browsers.nix
@@ -41,11 +41,6 @@ in {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
             installation_mode = "force_installed";
           };
-          # Dark Reader
-          "addon@darkreader.org" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
-            installation_mode = "force_installed";
-          };
           # Bitwarden
           "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
