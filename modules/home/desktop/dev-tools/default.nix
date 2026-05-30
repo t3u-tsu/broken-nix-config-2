013bdfec8978a483472057b627c34e447d1c65c5
t3u-tsu <t3u@t3u.uk>
2026-05-30 19:04:13 +0900

    chore: remove old parts

--- modules/home/desktop/dev-tools/default.nix
+++ modules/home/desktop/dev-tools/default.nix
@@ -11,7 +11,6 @@ in
 
   imports = [
     ./neovim.nix
-    ./vscode.nix
     ./git-tools.nix
     ./ai-tools.nix
     ./hardware.nix
@@ -22,7 +21,6 @@ in
     # Enable specific tools by default when the category is enabled
     my.home.desktop.dev-tools = {
       neovim.enable = mkDefault true;
-      vscode.enable = mkDefault false; # Use Neovim by default
       git-tools.enable = mkDefault true;
       ai-tools.enable = mkDefault true;
       hardware.enable = mkDefault true;
