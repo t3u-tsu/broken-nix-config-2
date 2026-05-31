d074dbffcce44f09a57c533d1a9507617bf2c9b9
t3u-tsu <t3u@t3u.uk>
2026-06-01 01:34:47 +0900

    fix(home/sops): add activation script to generate age keys.txt from SSH key

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,8 +1,12 @@
 { config, ... }:
 
+let
+  username = config.my.user.name;
+  systemStateVersion = config.system.stateVersion;
+in
 {
-  home-manager.users.${config.my.user.name} = {
-    home.stateVersion = config.system.stateVersion;
+  home-manager.users.${username} = { config, pkgs, ... }: {
+    home.stateVersion = systemStateVersion;
 
     imports = [
       ./shell.nix
@@ -20,9 +24,17 @@
     # SOPS configuration for Home-manager
     sops = {
       # 宣言的に ~/.config/sops/age/keys.txt に age 秘密鍵を自動生成・配置
-      age.keyFile = "/home/${config.my.user.name}/.config/sops/age/keys.txt";
-      age.sshKeyPaths = [ "/home/${config.my.user.name}/.ssh/id_ed25519" ];
+      age.keyFile = "/home/${username}/.config/sops/age/keys.txt";
+      age.sshKeyPaths = [ "/home/${username}/.ssh/id_ed25519" ];
       age.generateKey = false;
     };
+
+    # アクティベーション時に日常用 SSH 鍵から age 秘密鍵を安全に変換・出力する設定
+    home.activation.generateAgeKey = config.lib.dag.entryBetween [ "writeBoundary" ] [ "setupSecrets" ] ''
+      if [ ! -f "/home/${username}/.config/sops/age/keys.txt" ]; then
+        $DRY_RUN_CMD mkdir -p "/home/${username}/.config/sops/age"
+        $DRY_RUN_CMD ${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i "/home/${username}/.ssh/id_ed25519" > "/home/${username}/.config/sops/age/keys.txt" || true
+      fi
+    '';
   };
 }
