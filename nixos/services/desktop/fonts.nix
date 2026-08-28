b6bd5b8de2a01a611eade6505b50cae9c6bb3e29
t3u-tsu <t3u@t3u.uk>
2026-08-28 11:49:28 +0900

    refactor(desktop): add enable flag to fonts module

--- nixos/services/desktop/fonts.nix
+++ nixos/services/desktop/fonts.nix
@@ -1,29 +1,46 @@
-{ pkgs, ... }:
-
 {
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts-cjk-sans
-      noto-fonts-cjk-serif
-      noto-fonts-color-emoji
-      nerd-fonts.jetbrains-mono
-    ];
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
-    fontconfig = {
-      defaultFonts = {
-        serif = [
-          "Noto Serif CJK JP"
-          "Noto Color Emoji"
-        ];
-        sansSerif = [
-          "Noto Sans CJK JP"
-          "Noto Color Emoji"
-        ];
-        monospace = [
-          "JetBrainsMono Nerd Font"
-          "Noto Sans CJK JP"
-          "Noto Color Emoji"
-        ];
+with lib;
+let
+  cfg = config.my.services.desktop.fonts;
+in
+{
+  options.my.services.desktop.fonts = {
+    enable = mkEnableOption "System fonts" // {
+      default = true;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    fonts = {
+      packages = with pkgs; [
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
+        noto-fonts-color-emoji
+        nerd-fonts.jetbrains-mono
+      ];
+
+      fontconfig = {
+        defaultFonts = {
+          serif = [
+            "Noto Serif CJK JP"
+            "Noto Color Emoji"
+          ];
+          sansSerif = [
+            "Noto Sans CJK JP"
+            "Noto Color Emoji"
+          ];
+          monospace = [
+            "JetBrainsMono Nerd Font"
+            "Noto Sans CJK JP"
+            "Noto Color Emoji"
+          ];
+        };
       };
     };
   };
