e578ce8b0fd19ece02f5e0375c009d8a5f9e2077
t3u-tsu <t3u@t3u.uk>
2026-08-28 11:49:28 +0900

    refactor(desktop): split Thunar file manager config from niri

---
+++ nixos/services/desktop/thunar.nix
@@ -0,0 +1,37 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+with lib;
+let
+  cfg = config.my.services.desktop.thunar;
+in
+{
+  options.my.services.desktop.thunar = {
+    enable = mkEnableOption "Thunar file manager" // {
+      default = true;
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs = {
+      thunar = {
+        enable = true;
+        plugins = with pkgs; [
+          thunar-archive-plugin
+          thunar-volman
+        ];
+      };
+
+      xfconf.enable = true; # Required for Thunar settings persistence
+    };
+
+    services = {
+      gvfs.enable = true; # Required for file manager features (Trash, Mounts)
+      tumbler.enable = true; # Required for file manager thumbnails
+    };
+  };
+}
